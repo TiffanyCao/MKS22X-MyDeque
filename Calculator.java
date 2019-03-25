543e70fb69ab5751f03fc826f40ffaa7b4bee780
Tiffany Cao <tcao00@stuy.edu>
2019-03-25 13:39:39 -0400

    finished and tested calculator

--- Calculator.java
+++ Calculator.java
@@ -15,8 +15,7 @@ public class Calculator{
       //System.out.println(operationIndex);
       if(operationIndex == -1){
         calculate.addLast(Double.parseDouble(temp));
-      }
-      if(operationIndex != -1){
+      }else if(operationIndex != -1){
         double right = calculate.removeLast();
         double left = calculate.removeLast();
         if(operationIndex == 0){
@@ -25,13 +24,23 @@ public class Calculator{
         if(operationIndex == 1){
           calculate.addLast(left - right);
         }
+        if(operationIndex == 2){
+          calculate.addLast(left * right);
+        }
+        if(operationIndex == 3){
+          calculate.addLast(left / right);
+        }
       }
     }
-    return 0.0;
+    return calculate.getFirst();
   }
 
   public static void main(String[] args){
     String s = "1 2 +";
-    eval(s);
+    System.out.println(eval(s)); //is 3.0
+    System.out.println(eval("10 2.0 +")); //is 12.0
+    System.out.println(eval("11 3 - 4 + 2.5 *")); //is 30.0
+    System.out.println(eval("8 2 + 99 9 - * 2 + 9 -")); //is 893.0
+    System.out.println(eval("1 2 3 4 5 + * - -")); //is 26.0
   }
 }
